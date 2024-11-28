086b4d52e635b809a8e0b00fa559a435d516c143
Your Name <abdulaiabubakar473@gmail..com>
2024-11-28 05:50:23 -0600

    tax calculate well in invoice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_28_092654) do
+ActiveRecord::Schema.define(version: 2024_11_28_111014) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "resource_id", null: false
