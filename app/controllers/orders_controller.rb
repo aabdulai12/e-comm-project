01dd3993d63a317129ccaab4c93115bb6e8a16fe
Your Name <abdulaiabubakar473@gmail..com>
2024-12-04 13:25:03 -0600

    remove product  file added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user! # Ensure the user is logged in
+  before_action :authenticate_user!
 
   def index
     @orders = current_user.orders.includes(:order_items, :status).order(created_at: :desc)
