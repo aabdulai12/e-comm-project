c03a3a73155ec42ab9c6fa258957dde78c82d33a
Your Name <abdulaiabubakar473@gmail..com>
2024-12-03 20:33:18 -0600

    VALIDATE TITLE AND DESCRIPTION ADDED

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,6 +3,8 @@ class Status < ApplicationRecord
 
   has_many :orders, dependent: :destroy
 
+
+  # VALIDATE TITLE ADDED
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
 end
